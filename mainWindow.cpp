0acbc9c0a1948ec985b28081ad59e9a3cbf07b49
htkddt <huynhtuankietddt@gmail.com>
2025-04-19 14:39:41 +0700

    Init Windows basic

--- mainWindow.cpp
+++ mainWindow.cpp
@@ -1,8 +1,12 @@
 #include "mainWindow.h"
 
+#include "qpushbutton.h"
+#include "qtextedit.h"
+
 mainWindow::mainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    /* Init Windows components */
     setWindowTitle("Text-To-Speech Application");
 }
 
